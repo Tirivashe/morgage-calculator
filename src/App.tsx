502fb0ffec40177568a62912ad6091882b6004c9
Tirivashe <shaymusts@gmail.com>
2024-08-03 18:15:54 +0200

    Added visuals
    Added more color, font and layout for application. Font and image still
    not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Button } from '@mantine/core'
+import { Box, Text, Center, Flex, Group, Title } from '@mantine/core'
+import styles from "./App.module.css"
 
 function App() {
-
   return (
-    <>
-      <Button>This is a button</Button>
-    </>
+    <Center h="100%">
+      <Flex direction={{ base: "column", sm: "row" }} w="80%">
+        <Box px="md" py="lg" className={styles.left}>
+          <Group justify='space-between'>
+            <Title order={3}>Mortgage Calculator</Title>
+            <Text>Clear All</Text>
+          </Group>
+        </Box>
+        <Box className={styles.right}>
+          <img src="/images/illustration-empty.svg" alt="image" />
+        </Box>
+      </Flex>
+    </Center>
   )
 }
 
