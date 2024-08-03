76e64319d760951fe47056902f600ab7f8e3f3ae
Tirivashe <shaymusts@gmail.com>
2024-08-03 18:59:42 +0200

    Further improvements
    Added image and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Box, Text, Center, Flex, Group, Title } from '@mantine/core'
+import { Box, Text, Center, Flex, Group, Title, Stack } from '@mantine/core'
 import styles from "./App.module.css"
 
 function App() {
   return (
     <Center h="100%">
-      <Flex direction={{ base: "column", sm: "row" }} w="80%">
+      <Flex direction={{ base: "column", sm: "row" }} w="80%" bg="white">
         <Box px="md" py="lg" className={styles.left}>
           <Group justify='space-between'>
             <Title order={3}>Mortgage Calculator</Title>
@@ -12,7 +12,11 @@ function App() {
           </Group>
         </Box>
         <Box className={styles.right}>
-          <img src="/images/illustration-empty.svg" alt="image" />
+          <Stack gap="sm" p="md">
+            <img src="/images/illustration-empty.svg" alt="image" width="300" height="300" />
+            <Title ta="center" order={3} c="white">Results shown here</Title>
+            <Text ta="center" c="white">Complete the form and click "calculate repayments" to see what your monthly repayments should be</Text>
+          </Stack>
         </Box>
       </Flex>
     </Center>
